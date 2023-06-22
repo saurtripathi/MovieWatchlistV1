a2daf31f8e56c3d0d5c8dd14482245ea4d005692
Saurabh Tripathi <saurabh.umesh.tripathi@gmail.com>
2023-06-22 14:18:39 +0530

    Initial draft

--- index.js
+++ index.js
@@ -19,58 +19,61 @@ const posterImageArray = []
 const movieListDiv = document.getElementById('movie-list')
 
 
-
 document.getElementById('myForm').addEventListener('submit', async (event) => {
+
     event.preventDefault();
     movieListDiv.innerHTML = ''
     movieListDiv.classList.remove('movie-list-margin')
-    
+
     let html = ''
     const myFormData = new FormData(event.target)
     const searchParameter = myFormData.get('search')
     console.log(`${baseUrl}?i=${i}&apikey=${apikey}&t=${searchParameter}`)
     const response = await fetch(`${baseUrl}?i=${i}&apikey=${apikey}&s=${searchParameter}`)
     const data = await response.json()
-    if(data.Response === 'False'){
+    if (data.Response === 'False') {
         movieListDiv.innerHTML = `<div class="error-div" >
                                         <p>Unable to find what you’re looking for. Please try another search.</p>
                                 </div>`
-                                    
-                                
-    }else {
+
+
+    } else {
         data.Search.forEach(movie => titleArray.push(movie.Title))
-    console.log(titleArray)
+        console.log(titleArray)
 
-    titleArray.forEach(async title => {
-        const res = await fetch(`${baseUrl}?i=${i}&apikey=${apikey}&t=${title}`)
-        const d = await res.json()
-        let poster = d.Poster
-        let genre = d.Genre
-        let imdbRating = d.imdbRating
-        let plot = d.Plot
-        let runtime = d.Runtime
+        titleArray.forEach(async title => {
+            const res = await fetch(`${baseUrl}?i=${i}&apikey=${apikey}&t=${title}`)
+            const d = await res.json()
+            let poster = d.Poster
+            let genre = d.Genre
+            let imdbRating = d.imdbRating
+            let plot = d.Plot
+            let runtime = d.Runtime
 
-        html += `<div class="movie-card" >
-            <div><img src="${poster}" class="poster"/></div>
+            html += `<div class="movie-card" >
+                <div>
+                            <img src="${poster}" class="poster"/>
+                </div>
                 <div class="description" >
                     <div class="title" >
-                    <p class="movie-title" >${title}</p>
-                    <span class="fa fa-star checked"></span><span class="rating" >${imdbRating}</span>
+                            <p class="movie-title" >${title}</p>
+                            <span class="fa fa-star checked"></span>
+                            <span class="rating" >${imdbRating}</span>
                     </div>
                     <div class="genre" >
                             <span>${runtime}</span>
                             <span>${genre}</span>
                             <span><i class="fa-solid fa-circle-plus"></i>Watchlist</span>
-                            
-                           
                     </div>
-                    <div class="plot-content" ><p class="plot">${plot}</p></div>
-                </div>
+                    <div class="plot-content" >
+                            <p class="plot">${plot}</p>
+                    </div>
+                </div>   
             </div>`
-        movieListDiv.innerHTML = html
-    })
+            movieListDiv.innerHTML += html
+        })
     }
- 
+
 
 
 })
