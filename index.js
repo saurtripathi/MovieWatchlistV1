eef45e32e5c02329a0818c7dd2292f428bb25337
Saurabh Tripathi <saurabh.umesh.tripathi@gmail.com>
2023-06-25 14:28:40 +0530

    Final draft v1.0

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 // http://www.omdbapi.com/?i=tt3896198&apikey=2c13b038
-
 // http://www.omdbapi.com/?i=tt3896198&apikey=2c13b038&s=Blade Runner
-
 // https://www.omdbapi.com/?i=tt3896198&apikey=2c13b038&t=Blade+Runner&plot=full
-
 // http://www.omdbapi.com/?i=tt3896198&apikey=232da52c
 
 const baseUrl = 'https://www.omdbapi.com/'
-const i = 'tt3896198'
 const apikey = '232da52c'
-const titleArray = []
-const genreArray = []
-const plotArray = []
-const durationArray = []
-const ratingArray = []
-const posterImageArray = []
+const imdbIdArray = []
+const clickedToAddToWatchlistArray = []
+const movieArray = []
+let movieAddedToWatchlist = false
+let movie = {}
 
+localStorage.clear()
 const movieListDiv = document.getElementById('movie-list')
+const watchlistLnk = document.getElementById('watchlist')
 
 
 document.getElementById('myForm').addEventListener('submit', async (event) => {
@@ -28,49 +25,75 @@ document.getElementById('myForm').addEventListener('submit', async (event) => {
     let html = ''
     const myFormData = new FormData(event.target)
     const searchParameter = myFormData.get('search')
-    console.log(`${baseUrl}?i=${i}&apikey=${apikey}&t=${searchParameter}`)
-    const response = await fetch(`${baseUrl}?i=${i}&apikey=${apikey}&s=${searchParameter}`)
+    console.log(`${baseUrl}?apikey=${apikey}&s=${searchParameter}`)
+    const response = await fetch(`${baseUrl}?apikey=${apikey}&s=${searchParameter}`)
     const data = await response.json()
     if (data.Response === 'False') {
-        movieListDiv.innerHTML = `<div class="error-div" >
-                                        <p>Unable to find what you’re looking for. Please try another search.</p>
-                                </div>`
+        movieListDiv.classList.add('movie-list-margin')
+        movieListDiv.innerHTML = `
+                                // <div class="error-div">
+                                <p style="margin:0 1em;" >Unable to find what you’re looking for. Please try another search.</p>
+                                // </div> 
+                                
+                                `
 
 
     } else {
-        data.Search.forEach(movie => titleArray.push(movie.Title))
-        console.log(titleArray)
+        data.Search.forEach(movie => {
+            imdbIdArray.push(movie.imdbID)
+        })
+        console.log(imdbIdArray)
+        localStorage.setItem("imdbIdArray", JSON.stringify(imdbIdArray));
+        imdbIdArray.forEach(async id => {
+            const res = await fetch(`${baseUrl}?i=${id}&apikey=${apikey}`)
+            const data = await res.json()
+            let poster = data.Poster
+            console.log(poster)
+            let title = data.Title
+            let genre = data.Genre
+            let imdbRating = data.imdbRating
+            let plot = data.Plot
+            let runtime = data.Runtime
 
-        titleArray.forEach(async title => {
-            const res = await fetch(`${baseUrl}?i=${i}&apikey=${apikey}&t=${title}`)
-            const d = await res.json()
-            let poster = d.Poster
-            let genre = d.Genre
-            let imdbRating = d.imdbRating
-            let plot = d.Plot
-            let runtime = d.Runtime
+            if(poster === 'N/A')
+                poster = 'images/image-na.jpeg'
+
+            movie = {
+                'id'        :   id,
+                'title'     :   data.Title,
+                'poster'    :   poster,
+                'genre'     :   genre,
+                'imdbRating':   imdbRating,
+                'plot'      :   plot,
+                'runtime'   :   runtime
+            }
+
+            movieArray.push(movie)
+            // let imdbId = d.imdbId
+            console.log(movieArray)
+            localStorage.setItem("movieArray", JSON.stringify(movieArray));
 
             html += `<div class="movie-card" >
                 <div class="poster-div" >
-                            <img src="${poster}" class="poster"/>
+                            <img src="${poster}" class="poster" data-poster="${poster}"  />
                 </div>
                 <div class="description" >
                     <div class="title" >
-                            <p class="movie-title" >${title}</p>
+                            <p class="movie-title" data-title="${title}" >${title}</p>
                             <span class="fa fa-star checked"></span>
-                            <span class="rating" >${imdbRating}</span>
+                            <span class="rating" data-imdbRating="${imdbRating}" >${imdbRating}</span>
                     </div>
                     <div class="genre" >
-                            <span>${runtime}</span>
-                            <span>${genre}</span>
-                            <span><i class="fa-solid fa-circle-plus"></i>Watchlist</span>
+                            <span data-runtime="${runtime}" >${runtime}</span>
+                            <span data-genre="${genre}" >${genre}</span>
+                            <span><i class="fa-solid fa-circle-plus btn" data-watchlist="${id}"></i>Watchlist</span>
                     </div>
                     <div class="plot-content" >
-                            <p class="plot">${plot}</p>
+                            <p class="plot" data-plot="${plot}" >${plot}</p>
                     </div>
                 </div>   
             </div>`
-            movieListDiv.innerHTML += html
+            movieListDiv.innerHTML = html
         })
     }
 
@@ -79,5 +102,22 @@ document.getElementById('myForm').addEventListener('submit', async (event) => {
 })
 
 
+document.body.onclick = event => {
+
+    if(event.target.dataset.watchlist){
+        console.log(`event.target.dataset.watchlist : ${event.target.dataset.watchlist}`)
+    }
+
+    if(event.target.dataset.watchlist && !clickedToAddToWatchlistArray.includes(event.target.dataset.watchlist)){
+        // console.log(`clicked ${event.target.dataset.watchlist}`)
+
+        clickedToAddToWatchlistArray.push(event.target.dataset.watchlist)
+
+        localStorage.setItem("clickedToAddToWatchlistArray", JSON.stringify(clickedToAddToWatchlistArray));
+        
+        movieAddedToWatchlist = true
+
+    }
 
 
+}
